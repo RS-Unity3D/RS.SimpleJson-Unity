e38bc9809c13e5fa03cefdc05b0a9c7d3bdb7d80
RainsSoft <andyhebear@163.com>
2026-04-14 14:14:05 +0800

    更新.net4.0测试项目

--- Tests/RS.SimpleJsonUnityTest_Net40/Program.cs
+++ Tests/RS.SimpleJsonUnityTest_Net40/Program.cs
@@ -1,13 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+using RS.SimpleJsonUnity.Tests;
 namespace RS.SimpleJsonUnityTest_Net40
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            SimpleJsonConsoleTests.MainTest();
         }
     }
 }
