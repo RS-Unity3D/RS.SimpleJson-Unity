63e37b85ab97c97a5a92ead51f497ef80d6dd179
RainsSoft <andyhebear@163.com>
2026-04-14 14:13:48 +0800

    增加.net3.5测试程序

---
+++ Tests/RS.SimpleJsonUnityTest_Net35/Program.cs
@@ -0,0 +1,15 @@
+using RS.SimpleJsonUnity.Tests;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RS.SimpleJsonUnityTest_Net20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleJsonConsoleTests.MainTest();
+        }
+    }
+}
